34fa63b4e5852f7c742aace26ac67dc4623cbb2c
daisyzz <owencalleja13@gmail.com>
2017-02-20 20:22:36 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,17 +1,26 @@
+import time
+print("Welcome To HangMan :)")
 guess = (input("Player One Please Input Your Word For Player 2 Too Guess: "))
 guesses = []
 chances = 10
-print("The Word You Need To Guess is {} LettersLong".format(len(guess)))
+y = "y"
+n = "n"
+
+print("The Length of The Word You Need To Guess is {}".format(len(guess)))
 while True:
     if len(guesses) == len(guess):
-        print("You win")
-        print("Would you like to play again")
-        answer = input("Y/N: ")
-        if answer == "y" or "yes":
-            chances = (chances + 10)
+        print("You Win!!!!!!!!!")
+        print("Would You Like To Play Again")
+        answer = str(input("y/n: "))
+        if answer == y:
+            chances = 10
             guesses = []
             guess = (input("Player One Please Input Your Word For Player 2 Too Guess: "))
             continue
+        elif answer == n:
+            print("Bye :)")
+            time.sleep(5)
+            break
         else:
             break
     elif chances > 0:
@@ -22,11 +31,11 @@ while True:
     if y_guess in guess and y_guess not in guesses:
         print("You Guessed a letter")
         guesses.append(y_guess)
-        print("you have guessed {} letter".format(len(guesses)))
+        print("you have guessed {} letter's".format(len(guesses)))
         print(guesses)
     elif y_guess in guesses:
-        print("you already guessed that")
+        print("you already guessed that letter")
     else:
         chances = (chances - 1)
-        print("u Guessed wrong")
+        print("Your Guess is invalid")
         print("you have {} chances left".format(chances))
