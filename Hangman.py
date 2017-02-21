290e78cb7487bdcee2619373f71a54fd3ef91791
daisyzz <owencalleja13@gmail.com>
2017-02-21 19:16:42 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,41 +1,37 @@
-import time
-print("Welcome To HangMan :)")
-guess = (input("Player One Please Input Your Word For Player 2 Too Guess: "))
-guesses = []
-chances = 10
-y = "y"
-n = "n"
-
-print("The Length of The Word You Need To Guess is {}".format(len(guess)))
-while True:
-    if len(guesses) == len(guess):
-        print("You Win!!!!!!!!!")
-        print("Would You Like To Play Again")
-        answer = str(input("y/n: "))
-        if answer == y:
-            chances = 10
-            guesses = []
-            guess = (input("Player One Please Input Your Word For Player 2 Too Guess: "))
-            continue
-        elif answer == n:
-            print("Bye :)")
-            time.sleep(5)
-            break
-        else:
-            break
-    elif chances > 0:
-        y_guess = input("Player Two Please Enter Your Guess: ")
-    elif chances < 1:
-        print("You Lose")
-        break
-    if y_guess in guess and y_guess not in guesses:
-        print("You Guessed a letter")
-        guesses.append(y_guess)
-        print("you have guessed {} letter's".format(len(guesses)))
-        print(guesses)
-    elif y_guess in guesses:
-        print("you already guessed that letter")
-    else:
-        chances = (chances - 1)
-        print("Your Guess is invalid")
-        print("you have {} chances left".format(chances))
+def script():
+    import time
+    word = str(input("Please Enter a Word: "))
+    print("The Word's Length is {} Letter's :)".format(len(word)))
+    chances = 10
+    guesses = []
+    y = "Y"
+    n = "N"
+    while True:
+        if len(guesses) == len(word):
+            print("YOU WON!!!!!!!")
+            time.sleep(2.5)
+            replay = input("Would You Like To Play Again Y/N: ")
+            if replay == y:
+                script()
+            elif replay == n:
+                print("Okay Bye")
+                break
+            else:
+                print("Your Input Was Invalid")
+                break
+        elif chances > 0:
+            guess = input("Please Enter Your Guess: ")
+            if guess in word and guess not in guesses:
+                guesses.append(guess)
+                print("You Guessed {} So Far".format(len(guesses)))
+                print("You Have {} Left To Guess".format(len(word) - len(guesses)))
+            elif guess in guesses:
+                print("You Already Guessed That")
+            else:
+                chances = (chances - 1)
+                print("Your Guess Is Wrong")
+                print("You Have {} Chances Left".format(chances))
+        elif chances < 1:
+            print("You Lost :) Sorry")
+            script()
+script()
