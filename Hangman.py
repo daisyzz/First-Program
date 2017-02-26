60bbc5b546377b765667cab32074e0915e309d4e
daisyzz <owencalleja13@gmail.com>
2017-02-26 01:15:13 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,20 +1,20 @@
-#Defining a Function
 def script():
     #imported the time module for the time.sleep(seconds)
     import time
     import string
     import random
     print("Welcome to hangman :)")
-    list = ['pizza', 'love', 'daddy', 'stupid', 'fuck', 'jerk', 'asshole', 'chicken', 'nuggets', 'english', 'speakers']
-    word = (random.choice(list))
+    listz = ['pizza', 'love', 'daddy', 'stupid', 'fuck', 'jerk', 'asshole', 'chicken', 'nuggets', 'english', 'speakers']
+    word = (random.choice(listz))
     #Prints how long the word to guess is
     print("The word to guess is {} letter's :)".format(len(word)))
     chances = 10
     guesses = []
-    first = 1
-    while True:
-        #If guesses equals to word
-        if len(guesses) == len(word):
+    while chances > 0:
+        guess = input("Please enter a letter guess: ").lower()
+        if guess not in string.ascii_lowercase:
+            guess = input("Please enter a letter guess: ").lower()
+        elif len(guesses) == len(word):
             print("YOU WON!!!!!!!")
             time.sleep(2.5)
             replay = input("Would you like to play again Y/N: ").lower()
@@ -26,35 +26,31 @@ def script():
             else:
                 print("Your input was invalid")
                 break
-        elif first > 0:
-            guess = input("Please enter a letter guess: ").lower()
-            first = (first - 1)
-        elif chances > 0:
-            guess = input("Please enter your next guess: ").lower()
-            if guess not in string.ascii_lowercase:
-                guess = input("Please enter a letter guess: ").lower()
-        if guess in word and guess not in guesses:
-                #checks how many (guess) are in (word) and adds to (guesses)
-                for i in word:
-                    if i == guess:
-                        guesses.append(i)
-                    print(" {} ".format(i if i in guesses else "_"), end="")
-                print()
-                #if all guessed print........{
-                if len(guesses) == len(word):
-                    print("You Guessed The Word CONGRATS!!!!")
-                #else if len of guesses is not equal to word print ..........
-            #else if the guess is already guessed print.........
+        elif chances == 5:
+            left = ""
+            for letter in word:
+                if letter not in guesses:
+                    left = left + letter
+            hint = input("Would you like a hint Y/N: ").lower()
+            if hint == "y":
+                print("The hint is '{}'".format(random.choice(left)))
+        elif guess in word and guess not in guesses:
+            """checks how many (guess) are in (word) and adds to (guesses)"""
+            for i in word:
+                if i == guess:
+                    guesses.append(i)
+                print(" {} ".format(i if i in guesses else "_"), end="")
+            print()
+            """if all guessed print........"""
+            if len(guesses) == len(word):
+                print("You Guessed The Word CONGRATS!!!!")
+            """else if the guess is already guessed print........."""
         elif guess in guesses:
-                print("You Already Guessed That")
-            #else if guess is wrong decrease chances and print.....
-        else:
-                chances = (chances - 1)
-                print("Your Guess Is Wrong")
-                print("You Have {} Chances Left".format(chances))
-        #if chances are less then 1 prints......
-        if chances < 1:
-            print("You lost sorry")
-            script()
-#Calling a Function
+            print("You Already Guessed That")
+            """else if guess is wrong decrease chances and print....."""
+        elif guess not in word:
+            chances = (chances - 1)
+            print("You guessed wrong")
+            print("You have {} chances left".format(chances))
+            """if chances are less then 1 prints......"""
 script()
