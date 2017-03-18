605558745ca87469de221b4e1212861e0ab1fab0
daisyzz <owencalleja13@gmail.com>
2017-03-18 19:29:15 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,68 +1,59 @@
-def script():
-    #imported the time module for the time.sleep(seconds)
-    import time
-    import string
-    import random
-    print("Welcome To The Game Of HangMan")
-    list_guesses = ['pizza', 'love', 'daddy', 'stupid', 'fuck', 'jerk', 'asshole', 'chicken', 'nuggets', 'english', 'speakers']
-    word = (random.choice(list_guesses))
-    print("The word to guess is {} letter's :)".format(len(word)))
+import random
+import time
+
+
+def main():
+    # List of word's to use.
+    words = ['pizza', 'love', 'life', 'amaizing', 'china', 'phone', 'color', 'chicken']
+    # Choice's from the list above at random.
+    word = (random.choice(words))
+    # Prints the length of the word too guess.
+    print("The word im thinking about is {} letter's long".format(len(word)))
+    # Chances
     chances = 10
-    guesses = []
-    gussed = []
-    while chances > 0:
-        if chances != 5 and chances > 1 and len(guesses) != len(word):
-            guess = input("Please enter a letter guess: ").lower()
-        if guess not in string.ascii_lowercase or len(guess) > 1:
-            continue
-        if len(guesses) == len(word):
-            print("YOU WON!!!!!!!")
-            time.sleep(2.5)
-            replay = input("Would you like to play again Y/N: ").lower()
-            if replay == "y":
-                script()
-            elif replay == "n":
-                print("Okay Bye")
-                break
-            else:
-                print("Please Enter Y/N")
-        if chances == 5:
-            left = ""
-            for letter in word:
-                if letter not in guesses:
-                    left = left + letter
-            hint = input("Would you like a hint Y/N: ").lower()
-            if hint == "y":
-                hintz = format(random.choice(left))
-                for l in word:
-                    if l == hintz:
-                        guesses.append(l)
-                    print(" {} ".format(l if l in guesses else "_"), end="")
-                print()
-                print("The hint was {}".format(hintz))
-                chances = (chances - 1)
-                continue
-            elif hint == "n":
-                print("Okay good luck")
-                continue
-        if guess in word and guess not in guesses:
-            """checks how many (guess) are in (word) and adds to (guesses)"""
+    # Where the incorrect guess is stored.
+    done = []
+    # Where the correct guess is stored.
+    letters = []
+    # Loops untill ......
+    while chances > 0 and len(word) != len(letters):
+        # Ask's the user for his guess.
+        guess = str(input("Please input your guess: ")).lower()
+        # Check's if the guess is in word and if not already in letters.
+        if guess in word and guess not in letters:
+            # Finds the letter's from guess in word.
             for i in word:
+                # Make's i equal too guess.
                 if i == guess:
-                    guesses.append(i)
-                print(" {} ".format(i if i in guesses else "_"), end="")
+                    letters.append(i)
+                # Prints guessed letters in . else "-".
+                print("{}".format(i if i in letters else "-"), end="")
             print()
-            """if all guessed print........"""
-            if len(guesses) == len(word):
-                print("You Guessed The Word CONGRATS!!!!")
-            """else if the guess is already guessed print........."""
-        elif guess in guesses or guess in gussed:
-            print("You Already Guessed That")
-            """else if guess is wrong decrease chances and print....."""
+        # Check's if guess in done then print's.
+        elif guess in done:
+            print("You Guessed that already")
+        # Check's if guess not in word.
         elif guess not in word:
-            gussed.append(guess)
-            chances = (chances - 1)
-            print("You guessed wrong")
-            print("You have {} chances left".format(chances))
-            """if chances are less then 1 prints......"""
-script()
+            # Adds the incorrect guess into done.
+            done.append(guess)
+            # Subtracts chances by 1.
+            chances -= 1
+            # Print's.
+            print("You guessed wrong and have {} chance's left".format(chances))
+    # Check's if chances are equal to 0 then print's.
+    if chances == 0:
+        print("You Lost")
+    # If above is false print's......
+    else:
+        print("YOU WON!!!!!!!!!!!")
+    # Ask's user if he would like to replay.
+    replay = input("Please enter y to play again: ").lower()
+    # Check's if replay is equal to y
+    if replay == "y":
+        print("Enjoy!")
+        time.sleep(2.5)
+        main()
+    # If everything above false it break's
+    else:
+        print("Bye!")
+main()
