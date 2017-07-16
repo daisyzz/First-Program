14d46720d3a5806e120cf29a0cb7fed79f017ec1
daisyzz <owencalleja13@gmail.com>
2017-07-16 14:00:03 +0200

    Update time.py

--- time.py
+++ time.py
@@ -1,6 +1,7 @@
-choice = int(input("Would you like to convert 12/24 Hour time into military :"))
+choice = int(input("Would you like to convert 12/24 Hour time into military : "))
 
 m_time = []
+
 time = ""
 if choice == 24:
     time_24 = str(input("Please input 24H clock to convert to military : "))
@@ -8,7 +9,7 @@ if choice == 24:
         if i.isdigit() is True:
             m_time.append(i)
 elif choice == 12:
-    time_12 = str(input("Please input 12H clock to convert to military :"))
+    time_12 = str(input("Please input 12H clock to convert to military : "))
     for l in time_12:
         if l is "a":
             for i in time_12:
@@ -19,10 +20,10 @@ elif choice == 12:
                 if i.isdigit() is True:
                     i = int(i)
                     m_time.append(i)
-if choice == 12:
-    m_time.insert(0, 1)
-    m_time[1] += 2
-if len(m_time) < 4:
+            if len(m_time) == 4:
+                m_time.remove(0)
+            m_time[0] += 12
+if len(m_time) == 4 is False:
     m_time.insert(0, '0')
 m_time = map(str, m_time)
 m_time = "".join(m_time)
